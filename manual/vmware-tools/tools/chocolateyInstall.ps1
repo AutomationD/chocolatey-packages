3fd01323a5804924140d18c113547a42a1cef29d
tdgeery <taylorg@docstoc.com>
2015-02-25 14:39:40 -0800

    Updated package name

--- manual/vmware-tools/tools/chocolateyInstall.ps1
+++ manual/vmware-tools/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $url='http://packages.vmware.com/tools/esx/latest/windows/x86/VMware-tools-9.4.11-2400950-i386.exe'
 $url64='http://packages.vmware.com/tools/esx/latest/windows/x64/VMware-tools-9.4.11-2400950-x86_64.exe'
-$packageName = 'vmwaretools'
+$packageName = 'vmware-tools'
 $fileType = 'exe'
 $silentArgs = '/S /v /qn'
 
